4ebc58d927db3a89a64bd5ea05179fe051421bda
tylerkeyes <tyler.keyes00@gmail.com>
2024-03-25 21:39:45 -0400

    updates to server

---
+++ src/connection.rs
@@ -0,0 +1,25 @@
+use bytes::{Buf, BytesMut};
+use std::io::{self, Cursor};
+use tokio::io::{AsyncReadExt, AsyncWriteExt, BufWriter};
+use tokio::net::TcpStream;
+
+use crate::data::{data::deserialize, types::StoredType};
+
+#[derive(Debug)]
+pub struct Connection {
+    stream: BufWriter<TcpStream>,
+    buffer: BytesMut,
+}
+
+impl Connection {
+    pub fn new(socket: TcpStream) -> Connection {
+        Connection {
+            stream: BufWriter::new(socket),
+            buffer: BytesMut::with_capacity(4 * 1024),
+        }
+    }
+
+    pub async fn read_data(&mut self) -> Result<Option<StoredType>> {
+        let parsed_data = deserialize();
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,11 @@
-use axum::Router;
 use dotenv::dotenv;
-use hyper::Method;
 use log::info;
 use std::env;
+use tokio::net::TcpListener;
+use tokio::net::TcpStream;
 //use tokio::signal;
-use tower_http::cors::Any;
-use tower_http::cors::CorsLayer;
 
-use crate::controllers::service_controller;
-
+mod connection;
 mod controllers;
 mod data;
 
@@ -21,7 +18,43 @@ async fn main() {
 
     let app_development = env::var("APP_ENVIRONMENT").unwrap_or("development".to_string());
     let app_host = env::var("APP_HOST").unwrap_or("0.0.0.0".to_string());
-    let app_port = env::var("APP_PORT").unwrap_or("80".to_string());
+    let app_port = env::var("APP_PORT").unwrap_or("6379".to_string());
+    info!("Server running on {}:{}...", app_host, app_port);
+    let bind_address = app_host + ":" + &app_port;
+
+    match app_development.as_str() {
+        "development" => {
+            info!("running in development mode");
+        }
+        "production" => {
+            info!("running in production mode");
+        }
+        _ => {
+            info!("running in development mode");
+        }
+    }
+
+    let listener = TcpListener::bind(&bind_address).await.unwrap();
+
+    loop {
+        let (socket, _) = listener.accept().await.unwrap();
+        process(socket).await;
+    }
+}
+
+async fn process(socket: TcpStream) {}
+
+/*
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+    env_logger::init();
+
+    info!("Redis server starting...");
+
+    let app_development = env::var("APP_ENVIRONMENT").unwrap_or("development".to_string());
+    let app_host = env::var("APP_HOST").unwrap_or("0.0.0.0".to_string());
+    let app_port = env::var("APP_PORT").unwrap_or("6379".to_string());
 
     info!("Server running on {}:{}...", app_host, app_port);
 
@@ -41,7 +74,9 @@ async fn main() {
         .allow_methods([Method::GET, Method::POST])
         .allow_origin(Any);
 
-    let routes = Router::new().merge(service_controller::router().layer(cors));
+    let routes = Router::new()
+        .route("/:input", get(redis_input))
+        .merge(service_controller::router().layer(cors));
 
     let bind_address = app_host + ":" + &app_port;
     let listener = tokio::net::TcpListener::bind(&bind_address).await.unwrap();
@@ -49,6 +84,13 @@ async fn main() {
         .await
         .unwrap();
 }
+
+async fn redis_input(Path(input): Path<String>) -> Result<Response<axum::body::Body>, Infallible> {
+    println!("default handler with input: {}", input);
+    let res = Response::new(Body::new("response".to_string()));
+    Ok::<_, Infallible>(res)
+}
+*/
 
 /*
 #[allow(dead_code)]
